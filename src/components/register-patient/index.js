480d06964646610137c962d31175e59c9b46678f
Ana <aamaro@AT-5CD943HWRN.atsistemas.lan>
2020-01-27 09:34:31 +0100

    reorganized handle functions and data

--- src/components/register-patient/index.js
+++ src/components/register-patient/index.js
@@ -1,14 +1,46 @@
 import React from "react";
-const noop = () => {};
+import Title from "../title";
+import HandleForm from "../../form-patient";
 
-export default function RegisterPatient({ onSubmit = noop, name = "nombre" }) {
+export default function RegisterPatient({
+  onSubmit = HandleForm,
+  name = "Nombre ",
+  surname = "Apellidos ",
+  phone = "Teléfono ",
+  email = "Email ",
+  problem = "Descripción del problema: "
+}) {
   return (
-    <form onSubmit={onSubmit}>
-      <label htmlFor="nombre">
-        {name}
-        <input type="text" id="nombre"></input>
+    <>
+      <Title title={"Registro de Pacientes"}></Title>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="name">
+          {name}
+          <input type="text" name="name" id="name"></input>
+        </label>
+
+        <label htmlFor="surname">
+          {surname}
+          <input type="text" surname="surname" id="surname"></input>
+        </label>
+
+        <label htmlFor="phone">
+          {phone}
+          <input type="text" id="phone"></input>
+        </label>
+
+        <label htmlFor="email">
+          {email}
+          <input type="text" id="email"></input>
+        </label>
+
+        <label htmlFor="problem">
+          {problem}
+          <input type="text" id="problem"></input>
+        </label>
+
         <input type="submit"></input>
-      </label>
-    </form>
+      </form>
+    </>
   );
 }
--- src/form-patient/handle-form.js
+++ src/form-patient/handle-form.js
@@ -1,17 +1,27 @@
 import React from "react";
-import Title from "../components/title";
 import RegisterPatient from "../components/register-patient";
 
 export default function HandleForm(props) {
   const registerData = e => {
     e.stopPropagation();
     e.preventDefault();
-    console.log(e);
+    const name = e.target["name"].value;
+    const surname = e.target["surname"].value;
+    const phone = e.target["phone"].value;
+    const email = e.target["email"].value;
+    const problem = e.target["problem"].value;
+
+    console.log("name: " + name);
+    console.log("surname: " + surname);
+    console.log("phone: " + phone);
+    console.log("email: " + email);
+    console.log("problem" + problem);
+    console.log("%O", e.target);
     return false;
   };
+
   return (
     <>
-      <Title title={"Registro de Pacientes"}></Title>
       <RegisterPatient onSubmit={registerData}></RegisterPatient>
     </>
   );
