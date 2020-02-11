7d00b7cca033182a5b0ba99032021b0ea3ba4f71
Ana <aamaro@AT-5CD943HWRN.atsistemas.lan>
2020-02-11 09:23:08 +0100

    reorganized workspace

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import React from "react";
 import "./App.css";
 import HandleForm from "./components/form-patient";
 
-function App() {
+export default function App() {
   return (
     <>
       <HandleForm></HandleForm>
     </>
   );
 }
-
-export default App;
